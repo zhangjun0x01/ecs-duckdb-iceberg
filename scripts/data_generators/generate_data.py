20febb3e6a28fa50cefea1af911529d4a82fa3bf
Tmonster <tom@ebergen.com>
2025-02-19 10:51:36 +0100

    first generate rest data, then local data

--- scripts/data_generators/generate_data.py
+++ scripts/data_generators/generate_data.py
@@ -3,15 +3,17 @@ from generate_spark_rest.generate_iceberg_spark_rest import IcebergSparkRest
 
 # Example usage:
 if __name__ == "__main__":
-    # db = IcebergSparkLocal()
-    # conn = db.GetConnection()
-    # db.GenerateTables(conn)
-    # db.CloseConnection(conn)
-    # del db
-    # del conn
     db2 = IcebergSparkRest()
     conn2 = db2.GetConnection()
     db2.GenerateTables(conn2)
     db2.CloseConnection(conn2)
     del db2
     del conn2
+
+    db = IcebergSparkLocal()
+    conn = db.GetConnection()
+    db.GenerateTables(conn)
+    db.CloseConnection(conn)
+    del db
+    del conn
+
