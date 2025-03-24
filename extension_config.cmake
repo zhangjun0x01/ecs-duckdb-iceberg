6cd943dbd5297164e6d37b592539988f05b14aaf
Sam Ansmink <samansmink@hotmail.com>
2025-03-24 14:30:19 +0100

    load avro first

--- extension_config.cmake
+++ extension_config.cmake
@@ -1,15 +1,15 @@
 # This file is included by DuckDB's build system. It specifies which extension to load
 
+duckdb_extension_load(avro
+        LOAD_TESTS
+        GIT_URL https://github.com/duckdb/duckdb_avro
+        GIT_TAG aa2741544c5b53cc8fda6a85ca56bd0353116de4
+)
+
 # Extension from this repo
 duckdb_extension_load(iceberg
     SOURCE_DIR ${CMAKE_CURRENT_LIST_DIR}
     LOAD_TESTS
-)
-
-duckdb_extension_load(avro
-    LOAD_TESTS
-    GIT_URL https://github.com/duckdb/duckdb_avro
-    GIT_TAG aa2741544c5b53cc8fda6a85ca56bd0353116de4
 )
 
 duckdb_extension_load(tpch)
