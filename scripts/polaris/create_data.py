836a3bdb033031a14dd5063a8b0db3ff2746f8bf
Tmonster <tom@ebergen.com>
2025-04-03 15:02:27 +0200

    run make format-fix

--- scripts/polaris/create_data.py
+++ scripts/polaris/create_data.py
@@ -8,28 +8,32 @@ if client_id == '' or client_secret == '':
     print("no client_id or client_secret")
     exit(1)
 
-spark = (SparkSession.builder
-         .config("spark.sql.catalog.spark_catalog", "org.apache.iceberg.spark.SparkSessionCatalog")
-         .config("spark.jars.packages", "org.apache.iceberg:iceberg-spark-runtime-3.5_2.12:1.8.1,org.apache.hadoop:hadoop-aws:3.4.0,software.amazon.awssdk:bundle:2.23.19,software.amazon.awssdk:url-connection-client:2.23.19")
-         .config('spark.sql.iceberg.vectorization.enabled', 'false')
-         # Configure the 'polaris' catalog as an Iceberg rest catalog
-         .config("spark.sql.catalog.quickstart_catalog.type", "rest")
-         .config("spark.sql.catalog.quickstart_catalog", "org.apache.iceberg.spark.SparkCatalog")
-         # Specify the rest catalog endpoint
-         .config("spark.sql.catalog.quickstart_catalog.uri", "http://localhost:8181/api/catalog")
-         # Enable token refresh
-         .config("spark.sql.catalog.quickstart_catalog.token-refresh-enabled", "true")
-         # specify the client_id:client_secret pair
-         .config("spark.sql.catalog.quickstart_catalog.credential", f"{client_id}:{client_secret}")
-         # Set the warehouse to the name of the catalog we created
-         .config("spark.sql.catalog.quickstart_catalog.warehouse", "quickstart_catalog")
-         # Scope set to PRINCIPAL_ROLE:ALL
-         .config("spark.sql.catalog.quickstart_catalog.scope", 'PRINCIPAL_ROLE:ALL')
-         # Enable access credential delegation
-         .config("spark.sql.catalog.quickstart_catalog.header.X-Iceberg-Access-Delegation", 'vended-credentials')
-         .config("spark.sql.catalog.quickstart_catalog.io-impl", "org.apache.iceberg.io.ResolvingFileIO")
-         .config("spark.sql.catalog.quickstart_catalog.s3.region", "us-west-2")
-         .config("spark.history.fs.logDirectory", "/home/iceberg/spark-events")).getOrCreate()
+spark = (
+    SparkSession.builder.config("spark.sql.catalog.spark_catalog", "org.apache.iceberg.spark.SparkSessionCatalog")
+    .config(
+        "spark.jars.packages",
+        "org.apache.iceberg:iceberg-spark-runtime-3.5_2.12:1.8.1,org.apache.hadoop:hadoop-aws:3.4.0,software.amazon.awssdk:bundle:2.23.19,software.amazon.awssdk:url-connection-client:2.23.19",
+    )
+    .config('spark.sql.iceberg.vectorization.enabled', 'false')
+    # Configure the 'polaris' catalog as an Iceberg rest catalog
+    .config("spark.sql.catalog.quickstart_catalog.type", "rest")
+    .config("spark.sql.catalog.quickstart_catalog", "org.apache.iceberg.spark.SparkCatalog")
+    # Specify the rest catalog endpoint
+    .config("spark.sql.catalog.quickstart_catalog.uri", "http://localhost:8181/api/catalog")
+    # Enable token refresh
+    .config("spark.sql.catalog.quickstart_catalog.token-refresh-enabled", "true")
+    # specify the client_id:client_secret pair
+    .config("spark.sql.catalog.quickstart_catalog.credential", f"{client_id}:{client_secret}")
+    # Set the warehouse to the name of the catalog we created
+    .config("spark.sql.catalog.quickstart_catalog.warehouse", "quickstart_catalog")
+    # Scope set to PRINCIPAL_ROLE:ALL
+    .config("spark.sql.catalog.quickstart_catalog.scope", 'PRINCIPAL_ROLE:ALL')
+    # Enable access credential delegation
+    .config("spark.sql.catalog.quickstart_catalog.header.X-Iceberg-Access-Delegation", 'vended-credentials')
+    .config("spark.sql.catalog.quickstart_catalog.io-impl", "org.apache.iceberg.io.ResolvingFileIO")
+    .config("spark.sql.catalog.quickstart_catalog.s3.region", "us-west-2")
+    .config("spark.history.fs.logDirectory", "/home/iceberg/spark-events")
+).getOrCreate()
 
 
 spark.sql("USE quickstart_catalog")
@@ -37,12 +41,13 @@ spark.sql("SHOW NAMESPACES").show()
 
 spark.sql("CREATE NAMESPACE IF NOT EXISTS COLLADO_TEST")
 spark.sql("USE NAMESPACE COLLADO_TEST")
-spark.sql("""
+spark.sql(
+    """
 CREATE TABLE IF NOT EXISTS quickstart_table (
   id BIGINT, data STRING
 )
 USING ICEBERG
-""")
+"""
+)
 spark.sql("INSERT INTO quickstart_table VALUES (1, 'some data'), (2, 'more data'), (3, 'yet more data')")
 spark.sql("SELECT * FROM quickstart_table").show()
-
--- scripts/polaris/setup_polaris_catalog.py
+++ scripts/polaris/setup_polaris_catalog.py
@@ -5,35 +5,40 @@ from polaris.catalog.api_client import ApiClient as CatalogApiClient
 from polaris.catalog.api_client import Configuration as CatalogApiClientConfiguration
 
 # some of this is from https://github.com/apache/polaris/blob/e32ef89bb97642f2ac9a4db82252a4fcf7aa0039/getting-started/spark/notebooks/SparkPolaris.ipynb
-polaris_credential = 'root:s3cr3t' # pragma: allowlist secret
+polaris_credential = 'root:s3cr3t'  # pragma: allowlist secret
 
 client_id = os.getenv('POLARIS_ROOT_ID', '')
 client_secret = os.getenv('POLARIS_ROOT_SECRET', '')
 
 if client_id == '' or client_secret == '':
     Print("could not find polaris root id or polaris root secret")
-client = CatalogApiClient(CatalogApiClientConfiguration(username=client_id,
-                                                        password=client_secret,
-                                                        host='http://polaris:8181/api/catalog'))
+client = CatalogApiClient(
+    CatalogApiClientConfiguration(username=client_id, password=client_secret, host='http://polaris:8181/api/catalog')
+)
 
 oauth_api = IcebergOAuth2API(client)
-token = oauth_api.get_token(scope='PRINCIPAL_ROLE:ALL',
-                            client_id=client_id,
-                            client_secret=client_secret,
-                            grant_type='client_credentials',
-                            _headers={'realm': 'default-realm'})
+token = oauth_api.get_token(
+    scope='PRINCIPAL_ROLE:ALL',
+    client_id=client_id,
+    client_secret=client_secret,
+    grant_type='client_credentials',
+    _headers={'realm': 'default-realm'},
+)
 
 # create a catalog
 from polaris.management import *
 
-client = ApiClient(Configuration(access_token=token.access_token,
-                                 host='http://polaris:8181/api/management/v1'))
+client = ApiClient(Configuration(access_token=token.access_token, host='http://polaris:8181/api/management/v1'))
 root_client = PolarisDefaultApi(client)
 
 storage_conf = FileStorageConfigInfo(storage_type="FILE", allowed_locations=["file:///tmp"])
 catalog_name = 'polaris_demo'
-catalog = Catalog(name=catalog_name, type='INTERNAL', properties={"default-base-location": "file:///tmp/polaris/"},
-                  storage_config_info=storage_conf)
+catalog = Catalog(
+    name=catalog_name,
+    type='INTERNAL',
+    properties={"default-base-location": "file:///tmp/polaris/"},
+    storage_config_info=storage_conf,
+)
 catalog.storage_config_info = storage_conf
 root_client.create_catalog(create_catalog_request=CreateCatalogRequest(catalog=catalog))
 resp = root_client.get_catalog(catalog_name=catalog.name)
@@ -52,16 +57,20 @@ def create_principal(api, principal_name):
         else:
             raise e
 
+
 # Create a catalog role with the given name
 def create_catalog_role(api, catalog, role_name):
     catalog_role = CatalogRole(name=role_name)
     try:
-        api.create_catalog_role(catalog_name=catalog.name, create_catalog_role_request=CreateCatalogRoleRequest(catalog_role=catalog_role))
+        api.create_catalog_role(
+            catalog_name=catalog.name, create_catalog_role_request=CreateCatalogRoleRequest(catalog_role=catalog_role)
+        )
         return api.get_catalog_role(catalog_name=catalog.name, catalog_role_name=role_name)
     except ApiException as e:
         return api.get_catalog_role(catalog_name=catalog.name, catalog_role_name=role_name)
     else:
         raise e
+
 
 # Create a principal role with the given name
 def create_principal_role(api, role_name):
@@ -86,18 +95,24 @@ manager_catalog_role = create_catalog_role(root_client, catalog, "manage_catalog
 
 # Grant the catalog role to the principal role
 # All principals in the principal role have the catalog role's privileges
-root_client.assign_catalog_role_to_principal_role(principal_role_name=engineer_role.name,
-                                                  catalog_name=catalog.name,
-                                                  grant_catalog_role_request=GrantCatalogRoleRequest(catalog_role=manager_catalog_role))
+root_client.assign_catalog_role_to_principal_role(
+    principal_role_name=engineer_role.name,
+    catalog_name=catalog.name,
+    grant_catalog_role_request=GrantCatalogRoleRequest(catalog_role=manager_catalog_role),
+)
 
 # Assign privileges to the catalog role
 # Here, we grant CATALOG_MANAGE_CONTENT
-root_client.add_grant_to_catalog_role(catalog.name, manager_catalog_role.name,
-                                      AddGrantRequest(grant=CatalogGrant(catalog_name=catalog.name,
-                                                                         type='catalog',
-                                                                         privilege=CatalogPrivilege.CATALOG_MANAGE_CONTENT)))
+root_client.add_grant_to_catalog_role(
+    catalog.name,
+    manager_catalog_role.name,
+    AddGrantRequest(
+        grant=CatalogGrant(catalog_name=catalog.name, type='catalog', privilege=CatalogPrivilege.CATALOG_MANAGE_CONTENT)
+    ),
+)
 
 # Assign the principal role to the principal
-root_client.assign_principal_role(engineer_principal.principal.name, grant_principal_role_request=GrantPrincipalRoleRequest(principal_role=engineer_role))
-
-
+root_client.assign_principal_role(
+    engineer_principal.principal.name,
+    grant_principal_role_request=GrantPrincipalRoleRequest(principal_role=engineer_role),
+)
