8603b7ca46e1e8d17292be9592925708d4988829
Tishj <t_b@live.nl>
2025-04-10 13:18:19 +0200

    fix up the endpoint

--- src/iceberg_extension.cpp
+++ src/iceberg_extension.cpp
@@ -61,7 +61,7 @@ static void S3OrGlueAttachInternal(IcebergAttachOptions &input, const string &se
 
 	input.authorization_type = IRCAuthorizationType::SIGV4;
 	//! NOTE: The 'v1' here is for the AWS Service API version, not the Iceberg or IRC version
-	input.endpoint = StringUtil::Format("%s.%s.amazonaws.com/v1/iceberg", service, region);
+	input.endpoint = StringUtil::Format("%s.%s.amazonaws.com/iceberg", service, region);
 }
 
 static void S3TablesAttach(IcebergAttachOptions &input) {
