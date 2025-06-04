4c1cf0dd17f3dbd1243e64d552dd6c2f72a767bb
Tmonster <tom@ebergen.com>
2025-06-04 11:19:17 +0200

    bump avro

--- extension_config.cmake
+++ extension_config.cmake
@@ -4,7 +4,7 @@ duckdb_extension_load(icu)
 duckdb_extension_load(avro
         LOAD_TESTS
         GIT_URL https://github.com/duckdb/duckdb_avro
-        GIT_TAG ff766174cc6cc9c4ed93fc4b75871bcdffcc6e65
+        GIT_TAG ec4d3c007be073bfb30ab38367285daf621c4f11
 )
 
 # Extension from this repo
