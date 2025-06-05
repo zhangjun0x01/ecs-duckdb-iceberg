bb0b29a365f2a0823241cdacc09120ab7a3ce866
Tishj <t_b@live.nl>
2025-06-05 19:20:33 +0200

    missing files

---
+++ src/include/storage/authorization/none.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "storage/irc_authorization.hpp"
+
+namespace duckdb {
+
+class NoneAuthorization : public IRCAuthorization {
+public:
+	static constexpr const IRCAuthorizationType TYPE = IRCAuthorizationType::NONE;
+
+public:
+	NoneAuthorization();
+
+public:
+	static unique_ptr<IRCAuthorization> FromAttachOptions(IcebergAttachOptions &input);
+	unique_ptr<HTTPResponse> GetRequest(ClientContext &context, const IRCEndpointBuilder &endpoint_builder) override;
+};
+
+} // namespace duckdb
