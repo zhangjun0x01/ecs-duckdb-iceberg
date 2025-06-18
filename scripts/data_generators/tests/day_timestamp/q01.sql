57eeafdc45f833c7596ca44dc4de158b9954548a
Tishj <t_b@live.nl>
2025-06-18 23:52:13 +0200

    add support for day transform

---
+++ scripts/data_generators/tests/day_timestamp/q01.sql
@@ -0,0 +1,4 @@
+INSERT INTO default.day_timestamp VALUES
+    (TIMESTAMP_NTZ '2020-05-15 14:30:45', 12345, 'click'),
+    (TIMESTAMP_NTZ '2021-08-22 09:15:20', 67890, 'purchase'),
+    (TIMESTAMP_NTZ '2022-03-10 11:45:30', 54321, 'view');
---
+++ scripts/data_generators/tests/day_timestamptz/q01.sql
@@ -0,0 +1,4 @@
+INSERT INTO default.day_timestamptz VALUES
+    (TIMESTAMP '2020-05-15 14:30:45', 12345, 'click'),
+    (TIMESTAMP '2021-08-22 09:15:20', 67890, 'purchase'),
+    (TIMESTAMP '2022-03-10 11:45:30', 54321, 'view');
