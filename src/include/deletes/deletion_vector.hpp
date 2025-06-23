123b14a9c2070e14faf291a0cd27456f50232c1d
Tishj <t_b@live.nl>
2025-06-23 13:32:40 +0200

    remove json gunk

--- src/include/deletes/deletion_vector.hpp
+++ src/include/deletes/deletion_vector.hpp
@@ -3,46 +3,7 @@
 #include "duckdb/common/multi_file/multi_file_data.hpp"
 #include <roaring/roaring.hh>
 
-#include "yyjson.hpp"
-
-using namespace duckdb_yyjson;
-
 namespace duckdb {
-
-struct PuffinBlobMetadata {
-public:
-	PuffinBlobMetadata() {
-	}
-
-public:
-	static PuffinBlobMetadata FromJSON(yyjson_doc *doc, yyjson_val *obj);
-
-public:
-	string type;
-	vector<int32_t> fields;
-	int64_t snapshot_id = 0;
-	int64_t sequence_number = 0;
-	int64_t offset = 0;
-	int64_t length = 0;
-
-	bool has_compression_codec = false;
-	int64_t compression_codec = 0;
-
-	unordered_map<string, string> properties;
-};
-
-struct PuffinFileMetadata {
-public:
-	PuffinFileMetadata() {
-	}
-
-public:
-	static PuffinFileMetadata FromJSON(yyjson_doc *doc, yyjson_val *obj);
-
-public:
-	vector<PuffinBlobMetadata> blobs;
-	unordered_map<string, string> properties;
-};
 
 struct IcebergDeletionVector : public DeleteFilter {
 public:
